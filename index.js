73f4d7eaff8bab8c5a8e3e66bf67a706488be472
Jan-Kuta <jan.kuta@email.cz>
2017-02-25 19:20:53 +0100

    S3 support added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
-// var S3Adapter = require('parse-server').S3Adapter;
+var S3Adapter = require('parse-server').S3Adapter;
 var path = require('path');
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
@@ -15,12 +15,12 @@ var api = new ParseServer({
 	databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
 	cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
 	serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
-	
+
 	//**** Security Settings ****//
-	// allowClientClassCreation: process.env.CLIENT_CLASS_CREATION || false, 
+	// allowClientClassCreation: process.env.CLIENT_CLASS_CREATION || false,
 	appId: process.env.APP_ID || 'myAppId',
-	masterKey: process.env.MASTER_KEY || 'myMasterKey', //Add your master key here. Keep it secret!	
-	
+	masterKey: process.env.MASTER_KEY || 'myMasterKey', //Add your master key here. Keep it secret!
+
 	//**** Live Query ****//
 	// liveQuery: {
 	// 	classNames: ["TestObject", "Place", "Team", "Player", "ChatMessage"] // List of classes to support for query subscriptions
@@ -34,7 +34,7 @@ var api = new ParseServer({
 	/* Set the mount path as it is in serverURL */
 	// publicServerURL: process.env.SERVER_URL || 'http://localhost:1337/parse',
 	/* This will appear in the subject and body of the emails that are sent */
-	// appName: process.env.APP_NAME || "CodeCraft", 
+	// appName: process.env.APP_NAME || "CodeCraft",
 
 	// emailAdapter: {
 	// 	module: 'parse-server-simple-mailgun-adapter',
@@ -44,13 +44,13 @@ var api = new ParseServer({
 	// 		apiKey: process.env.MAILGUN_API_KEY  || "apikey"
 	// 	}
 	// },
-	
+
 	//**** File Storage ****//
-	// filesAdapter: new S3Adapter(
-	// 	{
-	// 		directAccess: true
-	// 	}
-	// )
+	filesAdapter: new S3Adapter(
+		{
+			directAccess: true
+		}
+	)
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
